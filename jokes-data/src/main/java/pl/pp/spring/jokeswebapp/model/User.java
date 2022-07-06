9de8f7d2e6cc4d7b0621a3d64690642a7f26e25e
Tomasz Kaczorkiewicz <bossandmaster69@gmail.com>
2022-07-06 13:47:19 +0200

    Add spring data and hibernate dependencies

---
+++ jokes-data/src/main/java/pl/pp/spring/jokeswebapp/model/User.java
@@ -0,0 +1,11 @@
+package pl.pp.spring.jokeswebapp.model;
+
+import javax.persistence.Entity;
+
+//@Entity
+public class User {
+    private long id;
+    private String userName;
+    private String email;
+    private String password;
+}
