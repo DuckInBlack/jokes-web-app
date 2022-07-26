08fb789b09340903e4e7d4e8ce81455af5c75495
Tomasz Kaczorkiewicz <bossandmaster69@gmail.com>
2022-07-26 21:22:21 +0200

    Add profile annotation

--- jokes-data/src/main/java/pl/pp/spring/jokeswebapp/services/map/UserMapService.java
+++ jokes-data/src/main/java/pl/pp/spring/jokeswebapp/services/map/UserMapService.java
@@ -1,10 +1,12 @@
 package pl.pp.spring.jokeswebapp.services.map;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import pl.pp.spring.jokeswebapp.model.User;
 import pl.pp.spring.jokeswebapp.services.UserService;
 
 @Service
+@Profile("map")
 public class UserMapService extends BaseMapService<User> implements UserService {
 
 }
