56ee309ff69493218349c2e5d8d8a0cc78c357f4
Tomasz Kaczorkiewicz <bossandmaster69@gmail.com>
2022-08-01 21:55:43 +0200

    Add services tests

---
+++ jokes-data/src/test/java/pl/pp/spring/jokeswebapp/services/db/UserDbServiceTest.java
@@ -0,0 +1,67 @@
+package pl.pp.spring.jokeswebapp.services.db;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.pp.spring.jokeswebapp.model.User;
+import pl.pp.spring.jokeswebapp.repositories.UserRepository;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserDbServiceTest {
+
+    @InjectMocks
+    private UserDbService userDbService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    private User user = new User();
+
+    @Test
+    void findAllShouldReturnEmpty() {
+        when(userRepository.findAll()).thenReturn(new HashSet<>());
+        List<User> users = userDbService.findAll();
+        assertEquals(0, users.size());
+    }
+
+    @Test
+    void findAllShouldReturnOneElement() {
+        when(userRepository.findAll()).thenReturn(Set.of(user));
+        List<User> users = userDbService.findAll();
+        assertEquals(1, users.size());
+    }
+
+    @Test
+    void findByNotExistId() {
+        when(userRepository.findById(anyLong())).thenReturn(Optional.empty());
+        User user = userDbService.findById(1L);
+        assertNull(user);
+    }
+
+    @Test
+    void findByExistId() {
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(user));
+        User user = userDbService.findById(1L);
+        assertNotNull(user);
+    }
+
+    @Test
+    void save() {
+
+        userDbService.save(user);
+        verify(userRepository).save(any(User.class));
+    }
+}
---
+++ jokes-data/src/test/java/pl/pp/spring/jokeswebapp/services/db/UserProfileDbServiceTest.java
@@ -0,0 +1,67 @@
+package pl.pp.spring.jokeswebapp.services.db;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.pp.spring.jokeswebapp.model.UserProfile;
+import pl.pp.spring.jokeswebapp.repositories.UserProfileRepository;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserProfileDbServiceTest {
+
+    @InjectMocks
+    private UserProfileDbService userProfileDbService;
+
+    @Mock
+    private UserProfileRepository userProfileRepository;
+
+    private UserProfile userProfile = new UserProfile();
+
+    @Test
+    void findAllShouldReturnEmpty() {
+        when(userProfileRepository.findAll()).thenReturn(new HashSet<>());
+        List<UserProfile> userProfiles = userProfileDbService.findAll();
+        assertEquals(0, userProfiles.size());
+    }
+
+    @Test
+    void findAllShouldReturnOneElement() {
+        when(userProfileRepository.findAll()).thenReturn(Set.of(userProfile));
+        List<UserProfile> userProfiles = userProfileDbService.findAll();
+        assertEquals(1, userProfiles.size());
+    }
+
+    @Test
+    void findByNotExistId() {
+        when(userProfileRepository.findById(anyLong())).thenReturn(Optional.empty());
+        UserProfile userProfile = userProfileDbService.findById(1L);
+        assertNull(userProfile);
+    }
+
+    @Test
+    void findByExistId() {
+        when(userProfileRepository.findById(anyLong())).thenReturn(Optional.of(userProfile));
+        UserProfile userProfile = userProfileDbService.findById(1L);
+        assertNotNull(userProfile);
+    }
+
+    @Test
+    void save() {
+
+        userProfileDbService.save(userProfile);
+        verify(userProfileRepository).save(any(UserProfile.class));
+    }
+}
